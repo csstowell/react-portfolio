486710136176ff7c044401c27072277d2ac0e078
Charlotte Stowell <charlottestowell@berkeley.edu>
2022-02-08 22:02:46 -0800

    Added img to data.js

--- src/data.js
+++ src/data.js
@@ -7,27 +7,29 @@ export const products = [
     },
     {
       id: 2,
-      img: "https://i.postimg.cc/jjg1sbKK/Full-Page-copy-1.png",
+      img: "https://i.postimg.cc/C5YKvhmT/ecom2.png",
       link: "https://github.com/csstowell",
     },
     {
       id: 3,
-      img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+      img: "https://i.postimg.cc/28bFWmVj/bootstrap.png",
       link: "https://github.com/csstowell",
     },
     {
       id: 4,
-      img: "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
+      img: "https://i.postimg.cc/BZzLFVLJ/bootstrap-Page2.png",
       link: "https://github.com/csstowell",
     },
     {
       id: 5,
-      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+      img: "https://i.postimg.cc/N0t02YLS/Screen-Shot-2022-02-08-at-10-00-05-PM.png",
       link: "https://github.com/csstowell",
     },
     {
       id: 6,
-      img: "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
       link: "https://github.com/csstowell",
     },
   ];
+
+  
